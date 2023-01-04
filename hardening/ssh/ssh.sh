5ca5a69ff845811aede113a89c0126a5ef3c4bf3
schm1d <36855679+schm1d@users.noreply.github.com>
2023-01-04 03:08:28 +0100

    Updated ssh.sh header

--- hardening/ssh/ssh.sh
+++ hardening/ssh/ssh.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#Script Name    : chroot.sh                                   
-#Description    : Fully encrypted LVM2 on LUKS with UEFI Arch installation script. 
-#               : This is the chroot which should be executed after the 'archinstall.sh'                                                 
+#Script Name    : ssh.sh                                   
+#Description    : A very secure sshd_config and ssh_config                                                 
 #Author         : @brulliant                                                
 #Linkedin       : https://www.linkedin.com/in/schmidbruno/    
 
