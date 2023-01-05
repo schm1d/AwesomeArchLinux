6a2dbb67e70b4b0c706272f13ad28c54266058c1
Bruno Schmid <schmid.github@gmail.com>
2023-01-05 19:41:51 +0100

    Added vim configuration file

---
+++ utils/vim.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+
+#Description    : This script configures vim
+#Author         : @brulliant
+#Linkedin       : https://www.linkedin.com/in/schmidbruno/
+
+# Install vim-plug package manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Create .vimrc file if it doesn't exist
+if [ ! -f ~/.vimrc ]; then
+    touch ~/.vimrc
+fi
+
+# Add vim-plug configuration to .vimrc file
+echo "call plug#begin('~/.vim/plugged')" >> ~/.vimrc
+echo "Plug 'sheerun/vimrc'" >> ~/.vimrc
+echo "Plug 'sheerun/vim-polyglot'" >> ~/.vimrc
+echo "Plug 'scrooloose/nerdtree'" >> ~/.vimrc 
+echo "Plug 'tpope/vim-fugitive'" >> ~/.vimrc 
+echo "Plug 'airblade/vim-gitgutter'" >> ~/.vimrc
+echo "call plug#end()" >> ~/.vimrc 
+
+ # Install plugins using vim-plug package manager 
+ vim +PlugInstall +qall
