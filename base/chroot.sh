1e0f1be003805a8a7815c2c7e0a6597df0041809
schm1d <36855679+schm1d@users.noreply.github.com>
2024-04-05 13:57:50 +0200

    Update chroot.sh
    Added /etc/issue file creation

--- base/chroot.sh
+++ base/chroot.sh
@@ -298,6 +298,56 @@ grub-mkconfig -o /boot/grub/grub.cfg &&\
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/efi --recheck &&\
 chmod 600 $LUKS_KEYS
 
+# Creating a cool /etc/issue
+echo -e "${BBlue}Creating Banner (/etc/issue).${NC}"
+
+cat > /etc/issue.net << EOF
+                     .ed"""" """\$\$\$\$be.
+                   -"           ^""**\$\$\$e.
+                 ."                   '\$\$\$c
+                /                      "4\$\$b
+               d  3                     \$\$\$\$
+               \$  *                   .\$\$\$\$\$\$
+              .\$  ^c           \$\$\$\$\$e\$\$\$\$\$\$\$\$.
+              d\$L  4.         4\$\$\$\$\$\$\$\$\$\$\$\$\$\$b
+              \$\$\$\$b ^ceeeee.  4\$\$ECL.F*\$\$\$\$\$\$\$
+  e\$""=.      \$\$\$\$P d\$\$\$\$F \$ \$\$\$\$\$\$\$\$\$- \$\$\$\$\$\$
+ z\$\$b. ^c     3\$\$\$F "\$\$\$\$b   \$"\$\$\$\$\$\$\$  \$\$\$\$*"      .=""\$c
+4\$\$\$\$L   \     \$\$P"  "\$\$b   .\$ \$\$\$\$\$...e\$\$        .=  e\$\$\$.
+^*\$\$\$\$\$c  %..   *c    ..    \$\$ 3\$\$\$\$\$\$\$\$\$\$eF     zP  d\$\$\$\$\$
+  "**\$\$\$ec   "\   %ce""    \$\$\$  \$\$\$\$\$\$\$\$\$\$*    .r" =\$\$\$\$P""
+        "*\$b.  "c  *\$e.    *** d\$\$\$\$\$"L\$\$    .d"  e\$\$***"
+          ^*\$\$c ^\$c \$\$\$      4J\$\$\$\$\$% \$\$\$ .e*".eeP"
+             "\$\$\$\$\$\$"'\$=e....\$*\$\$**\$cz\$\$" "..d\$*"
+               "*\$\$\$  *=%4.\$ L L\$ P3\$\$\$F \$\$\$P"
+                  "\$   "%*ebJLzb\$e\$\$\$\$\$b \$P"
+                    %..      4\$\$\$\$\$\$\$\$\$\$ "
+                     \$\$\$e   z\$\$\$\$\$\$\$\$\$\$%
+                      "*\$c  "\$\$\$\$\$\$\$P"
+                       ."""*\$\$\$\$\$\$\$\$bc
+                    .-"    .\$***\$\$\$"""*e.
+                 .-"    .e\$"     "*\$c  ^*b.
+          .=*""""    .e\$*"          "*bc  "*\$e..
+        .\$"        .z*"               ^*\$e.   "*****e.
+        \$\$ee\$c   .d"                     "*\$.        3.
+        ^*\$E")\$..\$"                         *   .ee==d%
+           \$.d\$\$\$*                           *  J\$\$\$e*
+            """""                             "\$\$\$"
+
+********************************************************************
+*                                                                  *
+* This system is for the use of authorized users only. Usage of    *
+* this system may be monitored and recorded by system personnel.   *
+*                                                                  *
+* Anyone using this system expressly consents to such monitoring   *
+* and is advised that if such monitoring reveals possible          *
+* evidence of criminal activity, system personnel may provide the  *
+* evidence from such monitoring to law enforcement officials.      *
+*                                                                  *
+********************************************************************
+EOF
+
+
 echo -e "${BBlue}Setting permission on config files...${NC}"
 
 chmod 0700 /boot
@@ -325,6 +375,8 @@ chmod 02750 /usr/bin/w
 chmod 02750 /usr/bin/who
 chmod 02750 /usr/bin/whereis
 chmod 0600 /etc/login.defs
+chown root:root /etc/issue
+chmod 644 /etc/issue
 
 echo -e "${BBlue}Setting root password...${NC}"
 passwd &&\
