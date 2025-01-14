f36878c2c4fb3fd5cdbc5ffeb3b7cfc1723e9552
schm1d <36855679+schm1d@users.noreply.github.com>
2025-01-14 21:43:58 +0100

    Adding nano configuration

--- base/chroot.sh
+++ base/chroot.sh
@@ -16,6 +16,30 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
+echo -e "${BBlue}Downloading the latest nanorc configuration file from Github...${NC}"
+curl -sL https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh -s -- -y
+
+# Add the following settings to the nano configuration file to harden it
+echo "set constantshow" >> ~/.nanorc
+echo "set locking" >> ~/.nanorc
+echo "set nohelp" >> ~/.nanorc
+echo "set nonewlines" >> ~/.nanorc
+echo "set nowrap" >> ~/.nanorc
+echo "set minibar" >> ~/.nanorc
+echo "set zap" >> ~/.nanorc
+echo "set linenumbers" >> ~/.nanorc
+echo "set tabsize 4" >> ~/.nanorc
+echo "set tabstospaces" >> ~/.nanorc
+echo "set wordbounds punct,alnum" >> ~/.nanorc
+echo "set regexp ^[A-Za-z_][A-Za-z0-9_]*$" >> ~/.nanorc
+
+# Enable and set a working backup directory
+set backup                              # Creates backups of your current file.
+set backupdir "~/.cache/nano/backups/"  # The location of the backups.
+
+# Set permissions on the configuration file to prevent unauthorized changes 
+chmod 600 ~/.nanorc
+
 # The below values will be changed by archinstall.sh
 DISK='<your_target_disk>'
 CRYPT_NAME='crypt_lvm'
