1d2a993dfb5304f280de6d99f4573cdde705cfee
schm1d <36855679+schm1d@users.noreply.github.com>
2025-01-20 00:28:59 +0100

    Added  WhiteSur-gtk-theme and GRUB2 theme
    Kudos to https://github.com/vinceliuice

--- utils/theme.sh
+++ utils/theme.sh
@@ -1,1 +1,34 @@
 
+#!/usr/bin/env bash
+
+# Description: This is the chroot script for Arch Linux installation.
+# Author: Bruno Schmid @brulliant
+# LinkedIn: https://www.linkedin.com/in/schmidbruno/
+
+set -euo pipefail
+
+# Set up the variables
+BBlue='\033[1;34m'
+NC='\033[0m'
+
+
+echo -e "${BBlue}installing WhiteSur Theme...${NC}"
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1
+
+cd ./WhiteSur-gtk-theme
+
+./install.sh --color dark --theme blue --gnomeshell -icon arch --roundedmaxwindow
+
+cd ../
+
+rm -rf ./WhiteSur-gtk-theme
+
+https://github.com/vinceliuice/grub2-themes
+
+cd ./grub2-themes
+
+sudo ./install.sh -t vimix -s ultrawide -i whitesur
+
+cd ../
+
+rm -rf ./grub2-themes
