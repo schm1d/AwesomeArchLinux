a952f06790b392066c8694d20b9c8ba3477cc093
schm1d <36855679+schm1d@users.noreply.github.com>
2025-09-24 23:48:12 +0200

    reverted to LUKS1

--- base/archinstall.sh
+++ base/archinstall.sh
@@ -436,13 +436,10 @@ log_action "Creating LUKS container"
 
 # Create LUKS container
 cryptsetup -v \
-    --type luks2 \
+    --type luks1 \
     --cipher aes-xts-plain64 \
     --key-size 512 \
     --hash sha512 \
-    --pbkdf argon2id \
-    --pbkdf-memory 4194304 \
-    --pbkdf-parallel 4 \
     --iter-time 3000 \
     --use-random \
     --verify-passphrase \
